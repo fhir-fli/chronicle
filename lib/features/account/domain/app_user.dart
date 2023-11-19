b6a5ce43be336c2b43841208189096bfcb32eb5b
Dokotela <grey.faulkenberry@mayjuun.com>
2023-11-19 18:49:00 -0500

    Adding atsign to app

--- lib/features/account/domain/app_user.dart
+++ lib/features/account/domain/app_user.dart
@@ -1,5 +1,17 @@
-class AppUser {
-  const AppUser({required this.uid, this.email, this.password});
+import 'package:at_client_mobile/at_client_mobile.dart';
+
+abstract class AppUser {
+  const AppUser();
+}
+
+class AtSignUser extends AppUser {
+  const AtSignUser({required this.atClient});
+
+  final AtClient atClient;
+}
+
+class FakeAppUser extends AppUser {
+  const FakeAppUser({required this.uid, this.email, this.password});
 
   final String uid;
   final String? email;
